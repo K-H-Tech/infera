810272e56cca66326e94115608f20d56e573405e
kave.haj <kave.haj@snapp.cab>
2025-11-16 02:48:11 +0330

    some changes

---
+++ services/user-dashboard/client/user_client.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"context"
+	"zarinpal-platform/core/logger"
+	"zarinpal-platform/core/trace"
+	"zarinpal-platform/services/user/domain"
+)
+
+// UserServiceClient defines the interface for client operations
+type UserServiceClient interface {
+	ExampleToDoMethod(ctx context.Context, param string) error
+}
+
+type userServiceClient struct {
+	userService domain.UserService
+}
+
+func NewUserServiceClient(userService domain.UserService) UserServiceClient {
+	return &userServiceClient{
+		userService: userService,
+	}
+}
+
+func (c *userServiceClient) ExampleToDoMethod(ctx context.Context, param string) error {
+	_, span := trace.GetTracer().Start(ctx, "ExampleServiceClient.ExampleToDoMethod")
+	defer span.End()
+
+	// Add implementation here
+	value, err := c.userService.ExampleMethodForDashboard(context.Background(), param)
+	if err != nil {
+		return err
+	}
+	logger.Log.Error("ExampleToDoMethod logger.", value)
+
+	return nil
+}

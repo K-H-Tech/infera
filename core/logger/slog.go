9ee2ec9a9fcadee21afee1a22946f77108c2282d
kave.haj <kave.haj@snapp.cab>
2025-11-16 15:28:50 +0330

    implement slog

--- core/logger/slog.go
+++ core/logger/slog.go
@@ -4,35 +4,34 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"sync"
 )
 
 type log struct {
 	logger *slog.Logger
 }
 
-var Log *log
+var (
+	Log  *log
+	once sync.Once
+)
 
 func InitLogger() {
-	// Create a new slog logger with TextHandler that mimics the logrus formatting
-	// logrus.SetFormatter(&logrus.TextFormatter{
-	//     DisableColors: false,
-	//     FullTimestamp: true,
-	//     ForceColors:   true,
-	// })
-	// logrus.SetLevel(logrus.DebugLevel)
-
-	// Configure slog with similar settings to logrus
-	opts := &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-	}
-
-	handler := slog.NewTextHandler(os.Stdout, opts)
-	Log = &log{
-		logger: slog.New(handler),
-	}
+	once.Do(func() {
+		opts := &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}
+		handler := slog.NewTextHandler(os.Stdout, opts)
+		Log = &log{
+			logger: slog.New(handler),
+		}
+	})
 }
 
-func (l *log) Debug(args ...interface{}) {
+func (l *log) Debug(args ...any) {
+	if l == nil || l.logger == nil {
+		return
+	}
 	if len(args) == 1 {
 		l.logger.Debug(fmt.Sprint(args[0]))
 	} else {
@@ -40,7 +39,17 @@ func (l *log) Debug(args ...interface{}) {
 	}
 }
 
-func (l *log) Info(args ...interface{}) {
+func (l *log) Debugf(format string, args ...any) {
+	if l == nil || l.logger == nil {
+		return
+	}
+	l.logger.Debug(fmt.Sprintf(format, args...))
+}
+
+func (l *log) Info(args ...any) {
+	if l == nil || l.logger == nil {
+		return
+	}
 	if len(args) == 1 {
 		l.logger.Info(fmt.Sprint(args[0]))
 	} else {
@@ -48,11 +57,17 @@ func (l *log) Info(args ...interface{}) {
 	}
 }
 
-func (l *log) Infof(format string, args ...interface{}) {
+func (l *log) Infof(format string, args ...any) {
+	if l == nil || l.logger == nil {
+		return
+	}
 	l.logger.Info(fmt.Sprintf(format, args...))
 }
 
-func (l *log) Warn(args ...interface{}) {
+func (l *log) Warn(args ...any) {
+	if l == nil || l.logger == nil {
+		return
+	}
 	if len(args) == 1 {
 		l.logger.Warn(fmt.Sprint(args[0]))
 	} else {
@@ -60,11 +75,17 @@ func (l *log) Warn(args ...interface{}) {
 	}
 }
 
-func (l *log) Warnf(format string, args ...interface{}) {
+func (l *log) Warnf(format string, args ...any) {
+	if l == nil || l.logger == nil {
+		return
+	}
 	l.logger.Warn(fmt.Sprintf(format, args...))
 }
 
-func (l *log) Error(args ...interface{}) {
+func (l *log) Error(args ...any) {
+	if l == nil || l.logger == nil {
+		return
+	}
 	if len(args) == 1 {
 		l.logger.Error(fmt.Sprint(args[0]))
 	} else {
@@ -72,11 +93,18 @@ func (l *log) Error(args ...interface{}) {
 	}
 }
 
-func (l *log) Errorf(format string, args ...interface{}) {
+func (l *log) Errorf(format string, args ...any) {
+	if l == nil || l.logger == nil {
+		return
+	}
 	l.logger.Error(fmt.Sprintf(format, args...))
 }
 
-func (l *log) Fatal(args ...interface{}) {
+func (l *log) Fatal(args ...any) {
+	if l == nil || l.logger == nil {
+		os.Exit(1)
+		return
+	}
 	if len(args) == 1 {
 		l.logger.Error(fmt.Sprint(args[0]))
 	} else {
@@ -85,7 +113,11 @@ func (l *log) Fatal(args ...interface{}) {
 	os.Exit(1)
 }
 
-func (l *log) Fatalf(format string, args ...interface{}) {
+func (l *log) Fatalf(format string, args ...any) {
+	if l == nil || l.logger == nil {
+		os.Exit(1)
+		return
+	}
 	l.logger.Error(fmt.Sprintf(format, args...))
 	os.Exit(1)
 }
